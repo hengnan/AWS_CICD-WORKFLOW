ad266647905dee3f2114110f28fc194838cd1540
hengnan <hengnanma@gmail.com>
2025-03-30 23:10:19 -0400

    added remote backend modules and configured main tf file

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "backend" {
+  source                                              = "./modules/remote_backend"
+  iam_user_name                                       = var.iam_user_name
+  bucket_name                                         = var.bucket_name
+  table_name                                          = var.table_name
+}
