dc8446bcc94844423b8a31558cef4135bba0d172
hengnan <hengnanma@gmail.com>
2025-03-30 23:18:48 -0400

    added remote backend modules and configured main tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,3 @@ variable "region" {}
 variable "iam_user_name" {}
 variable "bucket_name" {}
 variable "table_name" {}
-variable "root_domain" {}
-variable "dns_record_ttl" {}
-variable "website_bucket" {}
-variable "force_destroy" {}
-variable "versioning_enabled" {}
-variable "index_document" {}
