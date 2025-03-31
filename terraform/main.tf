eb3c51c7ca82779ccb9aea1cbfe7324256bf87af
hengnan <hengnanma@gmail.com>
2025-03-31 11:19:55 -0400

    added s3 static website syncing into the workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,23 @@ provider "aws" {
 }
 
 module "backend" {
-  source        = "./modules/remote_backend"
-  iam_user_name = var.iam_user_name
-  bucket_name   = var.bucket_name
-  table_name    = var.table_name
+  source                                              = "./modules/remote_backend"
+  iam_user_name                                       = var.iam_user_name
+  bucket_name                                         = var.bucket_name
+  table_name                                          = var.table_name
 }
 
 module "dns_acm" {
-  source         = "./modules/route53"
-  root_domain    = var.root_domain
-  dns_record_ttl = var.dns_record_ttl
+  source                                              = "./modules/route53"
+  root_domain                                         = var.root_domain
+  dns_record_ttl                                      = var.dns_record_ttl
+}
+
+module "s3_website" {
+  source                                              = "./modules/s3_website"
+  website_bucket                                      = var.website_bucket
+  force_destroy                                       = var.force_destroy
+  versioning_enabled                                  = var.versioning_enabled
+  index_document                                      = var.index_document
+  region                                              = var.region
 }
